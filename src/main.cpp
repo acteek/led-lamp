a03b59112a45118c0cc5d453f5c5b1172bf640c3
Sergey Ryazanov <sergey.ryazanov@nordigy.ru>
2021-05-12 23:44:02 +0300

    use blt

--- src/main.cpp
+++ src/main.cpp
@@ -1,150 +1,86 @@
 #include <Arduino.h>
-#include <IRremote.h>
 #include <FastLED.h>
+#include <SoftwareSerial.h>
 
-//Set arduino PIN
-#define IR_RECEIVE_PIN 5
+//Setup
+#define RX_PIN 10
+#define TX_PIN 11
 #define LED_PIN 2
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
-#define NUM_LEDS 5 //85
+#define NUM_LEDS 85
 #define BRIGHTNESS 100
 #define FRAMES_PER_SECOND 120
 
-//Init libs struct
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-IRrecv irrecv(IR_RECEIVE_PIN);
-decode_results results;
+uint8_t currentHue = 0;                  // color HUE value
+uint8_t currentPatternNumber = 0;        // current pattern index
+boolean isLedOn = true;                  // lighting led flag
+SoftwareSerial BTSerial(RX_PIN, TX_PIN); // init BT UART port
 CRGB leds[NUM_LEDS];
+typedef void (*SimplePatternList[])();
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(IR_RECEIVE_PIN, INPUT);
+
   pinMode(LED_PIN, OUTPUT);
-
   Serial.begin(115200);
-  irrecv.enableIRIn(); // Start the receiver
+  BTSerial.begin(9600); // 38400 for setting mode
+  BTSerial.setTimeout(1000);
+  delay(2000); // wait for init UART
 
-  delay(3000); // 3 second delay for recovery
-  // tell FastLED about the LED strip configuration
+  // init led strip
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
+
+  Serial.println("Led has been inited...");
 }
 
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0;                  // rotating "base color" used by many of the patterns
-boolean is_led_off = false;        // rotating "base color" used by many of the patterns
+//--- Start describe patterns
 
 void rainbow()
 {
   // FastLED's built-in rainbow generator
-  fill_rainbow(leds, NUM_LEDS, gHue, 7);
+  fill_rainbow(leds, NUM_LEDS, currentHue, 7);
 }
 
-void addGlitter(fract8 chanceOfGlitter)
-{
-  if (random8() < chanceOfGlitter)
-  {
-    leds[random16(NUM_LEDS)] += CRGB::White;
-  }
-}
-
-void rainbowWithGlitter()
-{
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
-}
-
-void confetti()
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy(leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV(gHue + random8(64), 200, 255);
-}
-
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy(leds, NUM_LEDS, 20);
-  int pos = beatsin16(13, 0, NUM_LEDS - 1);
-  leds[pos] += CHSV(gHue, 255, 192);
-}
-
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8(BeatsPerMinute, 64, 255);
-  for (int i = 0; i < NUM_LEDS; i++)
-  { //9948
-    leds[i] = ColorFromPalette(palette, gHue + (i * 2), beat - gHue + (i * 10));
-  }
-}
-
-void juggle()
-{
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy(leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    leds[beatsin16(i + 7, 0, NUM_LEDS - 1)] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-}
-
-SimplePatternList gPatterns = {rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm};
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE(gPatterns);
-}
+SimplePatternList patterns = {rainbow};
+//--- Stop describe patterns
 
 void loop()
 {
-  if (irrecv.decode(&results))
-  {
-    Serial.println(results.value, HEX);
-    irrecv.resume(); // Receive the next value
 
-    switch (results.value)
+  if (BTSerial.available())
+  {
+
+    int command = BTSerial.parseInt();
+
+    String message = "Recieve msg: ";
+    message += command;
+    Serial.println(message);
+
+    switch (command)
     {
-    case 0x488F3CBB:
-      nextPattern();
+    case 99:
+      isLedOn = !isLedOn;
       break;
-    case 0x3D9AE3F7:
-      FastLED.clear(true);
-      is_led_off = true;
-      break;
-    case 0x1BC0157B:
-      is_led_off = false;
-      break;  
     default:
+      if (command < sizeof(patterns))
+      {
+        currentPatternNumber = command;
+      }
       break;
     }
   }
 
-  if (!is_led_off)
+  if (isLedOn)
   {
     // Call the current pattern function once, updating the 'leds' array
-    gPatterns[gCurrentPatternNumber]();
+    patterns[currentPatternNumber]();  // delay(300); // delay to parse ir signal
 
     // send the 'leds' array out to the actual LED strip
     FastLED.show();
-    // insert a delay to keep the framerate modest
-    FastLED.delay(1000 / FRAMES_PER_SECOND);
-
-    // do some periodic updates
-    EVERY_N_MILLISECONDS(20) { gHue++; } // slowly cycle the "base color" through the rainbow
+    FastLED.delay(1000 / FRAMES_PER_SECOND);   // insert a delay to keep the framerate modest
+    EVERY_N_MILLISECONDS(20) { currentHue++; } // slowly cycle the "base color" through the rainbow
   }
-  delay(300); // delay to parse ir signal
+
 }
